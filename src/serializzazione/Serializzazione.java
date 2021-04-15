9f6aa78e2da0f4c31b48df2a4d3fa0e7757cd26c
manduca.fabio <manduca.fabio@TAVE-PC18.OSDBSESTO.local>
2021-04-15 13:17:50 +0200

    creation of file .ser and declaration of stream

--- src/serializzazione/Serializzazione.java
+++ src/serializzazione/Serializzazione.java
@@ -5,9 +5,18 @@
  */
 package serializzazione;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author manduca.fabio
+ * OBJECT INPUT STREAM -->LEGGO
+ * OBJECT OUTPUT STREAM -->SCRIVO
  */
 public class Serializzazione {
 
@@ -15,7 +24,32 @@ public class Serializzazione {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //Oggetto da serializzare
+       Studente s = new Studente("Fabio","Manduca",12345);
+       
+       
+        try {
+            
+          
+            
+            FileOutputStream fos = new FileOutputStream("TEST.ser");
+            
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+               
+               
+               
+               
+               
+               
+               
+           } catch (IOException ex) {
+               Logger.getLogger(Serializzazione.class.getName()).log(Level.SEVERE, null, ex);
+               System.err.println("broken java");
+    }
+       
+       
+       
     }
     
 }
---
+++ src/serializzazione/Studente.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serializzazione;
+
+/**
+ *
+ * @author manduca.fabio
+ */
+public class Studente {
+    private String nome;
+    private String cognome;
+    private int nRegistro;
+
+    public Studente(String nome, String cognome, int nRegistro) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.nRegistro = nRegistro;
+    }
+    
+}
